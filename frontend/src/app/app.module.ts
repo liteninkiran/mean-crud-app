f08b526a551d85921d39c9707b0b53d4fb858853
Kiran Anand <liteninkiran@hotmail.com>
2021-09-11 16:10:12 +0100

    Routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee/employee.component';
 import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { EmployeeComponent } from './employee/employee/employee.component';
+import { AddEmployeeComponent } from './employee/add-employee/add-employee.component';
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: EmployeeComponent,
+    },
+    {
+        path: 'add-employee',
+        component: AddEmployeeComponent,
+    },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EmployeeComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        EmployeeComponent,
+        AddEmployeeComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes),
+    ],
+    providers: [
+
+    ],
+    bootstrap: [
+        AppComponent,
+    ],
 })
+
 export class AppModule { }
